e100af3d5eec9bbbb044a4d9802f0b851364bdb9
Saurabh Barasiya <43789500+Saurabh1Barasiya@users.noreply.github.com>
2021-04-28 16:24:54 +0530

    Add files via upload

---
+++ javascript_program/js/part_3.js
@@ -0,0 +1,36 @@
+// learn about data types.
+
+/*
+Primitive data types are numbers, Booleans, strings, undefined, null, and symbol. 
+*/
+
+
+let number = 20 ;
+let name = "Saurabh";
+let isMale = true;
+
+console.log(typeof number,typeof name,typeof isMale);
+
+let arr = [10,20,30,40];
+let myDict = {name:"Saurabh",number:20,isMale:true};    
+console.log(typeof arr);  //object
+console.log(myDict); //
+console.log(typeof myDict); //
+
+
+let number1 = 34;  // number
+let number2 = 34.4; // number
+console.log(number1,number2);
+console.log(typeof number1,typeof number2);
+
+function nameFunc(params) {
+    
+}
+console.log( nameFunc);
+console.log(typeof nameFunc);
+
+let obj = new Date();
+console.log( obj);
+console.log(typeof obj);
+
+
