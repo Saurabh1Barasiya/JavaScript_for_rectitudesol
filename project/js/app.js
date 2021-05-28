7b69a583c78323832e7f8e263d816cb4209f335a
Saurabh Barasiya <43789500+Saurabh1Barasiya@users.noreply.github.com>
2021-05-28 19:02:32 +0530

    Add files via upload

---
+++ project/js/app.js
@@ -0,0 +1,109 @@
+console.log("welcome to project");
+swowNotes();
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click',function(e) {
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(addTxt.value);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTxt.value = "";
+    console.log(notesObj);
+
+    swowNotes();
+})
+
+
+
+// function to show elements from localStorage.
+function swowNotes(){
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+
+    let html = "";
+    notesObj.forEach(function(element, index){
+        html += `<div class="card noteCards my-2 mx-2" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Note ${index+1}</h5>
+            <p class="card-text">${element}</p>
+            <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+        </div>
+    </div>`
+    });
+    let notesElm = document.getElementById("notes");
+    if (notesObj.length != 0){
+        notesElm.innerHTML = html;
+    }else{
+        notesElm.innerHTML = `Nothing to show use "Add a note" section above to add notes.`
+    }  
+}
+
+
+// function to delete a note
+function deleteNote(index){
+    console.log("I am deleting",index);
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+
+    notesObj.splice(index,1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+
+    swowNotes();
+    
+}
+
+// this.id likhne se usi element ki id this keyword par aa jati h jis element par click kiya jata h.
+
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function(){
+
+    let inputVal = search.value.toLowerCase();
+    // console.log('Input event fired!', inputVal);
+    let noteCards = document.getElementsByClassName('noteCards');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+        // console.log(cardTxt);
+    })
+})
+
+
+
+// search = document.getElementById('searchTxt');
+// search.addEventListener('input', function(){
+//     let inputVal = search.value.toLowerCase();
+//     console.log('Input event fired.',inputVal);
+//     let noteCards = document.getElementById('noteCards');
+//     Array.from(noteCards).forEach(function(element){
+//         let cardTxt = element.getElementsByTagName('p')[0].innerText;
+//         if(cardTxt.includes(inputVal)){
+//             element.style.display = 'block';
+//         }else{
+//             element.style.display = 'none';
+//         }
+//         console.log(cardTxt);
+//     })
+    
+// })
